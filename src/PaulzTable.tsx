9785ec21d5d2216a90ddfec462bd9459dc81211b
paulz <z0rge@bk.ru>
2024-12-25 10:16:43 +0300

    add Styles.tsx component

--- src/PaulzTable.tsx
+++ src/PaulzTable.tsx
@@ -1,15 +1,16 @@
 import React, { FC, createRef } from 'react';
 import {StraightTable} from "./components/tables/StraightTable";
 import {StraightTableTransformedProps} from "./types/StraightTableTypes";
+import Styles from "./Styles";
 
 const PaulzTable: FC<StraightTableTransformedProps> = props => {
   const { columns, data, height, width } = props;
   const rootElem = createRef<HTMLDivElement>();
 
   return (
-      <div ref={rootElem} style={{ width: width, height: height }}>
+      <Styles ref={rootElem} style={{ width, height }}>
         <StraightTable columns={columns} data={data}/>
-      </div>
+      </Styles>
   );
 };
 
